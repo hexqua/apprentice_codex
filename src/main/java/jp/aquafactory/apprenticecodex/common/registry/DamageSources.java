35cea8423c0229d8db2b0f7aa24050e8cc5bdfdf
Aquid <hexqua.limit@gmail.com>
2026-01-24 21:00:15 +0900

    エンダードラゴンにダメージが通らない理由が実装ミスだったため、修正してタグ削除

--- src/main/java/jp/aquafactory/apprenticecodex/common/registry/DamageSources.java
+++ src/main/java/jp/aquafactory/apprenticecodex/common/registry/DamageSources.java
@@ -9,7 +9,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("removal")
 public class DamageSources {
@@ -17,12 +16,12 @@ public class DamageSources {
         // do nothing.
     }
 
-    public static DamageSource getDamageSource(Level level, Entity projectile, @Nullable Entity owner, String typeName) {
+    public static DamageSource getDamageSource(Level level, Entity projectile, Entity owner, String typeName) {
         var reg = level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE);
         var type = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(ApprenticeCodex.MODID, typeName));
         var holder = reg.getHolder(type)
                 .orElseGet(() -> (Holder.Reference<DamageType>) level.damageSources().genericKill().typeHolder());
 
-        return new DamageSource(holder, owner, projectile);
+        return new DamageSource(holder, projectile, owner);
     }
 }
