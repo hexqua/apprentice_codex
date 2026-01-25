c03d31ee464ce2118bcd8abdee86c7dfef118e89
Aquid <hexqua.limit@gmail.com>
2026-01-25 12:12:33 +0900

    追従の仕組みを追加

--- src/main/java/jp/aquafactory/apprenticecodex/common/spells/archermultiple/ArcherMultipleBowEntity.java
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/archermultiple/ArcherMultipleBowEntity.java
@@ -2,11 +2,13 @@ package jp.aquafactory.apprenticecodex.common.spells.archermultiple;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,16 +23,18 @@ public class ArcherMultipleBowEntity extends Entity implements TraceableEntity {
 
     public ArcherMultipleBowEntity(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        setNoGravity(true);
     }
 
     public ArcherMultipleBowEntity(EntityType<?> pEntityType, Level pLevel, LivingEntity owner) {
         super(pEntityType, pLevel);
         setOwner(owner);
+        setNoGravity(true);
     }
 
     @Override
     protected void defineSynchedData() {
-
+        // do nothing.
     }
 
     @Override
@@ -68,6 +72,51 @@ public class ArcherMultipleBowEntity extends Entity implements TraceableEntity {
             ownerUUID = pOwner.getUUID();
             cachedOwner = pOwner;
         }
+    }
 
+    @Override
+    public void tick(){
+        super.tick();
+
+        @SuppressWarnings("resource") var level = level();
+        if(level.isClientSide) {
+            return;
+        }
+
+        if(!(getOwner() instanceof LivingEntity owner)) {
+            discard();
+            return;
+        }
+        
+        var targetPosition = computeBehindPos(owner, 0.75, -1.0, 1.0);
+        var targetVec = targetPosition.subtract(position());
+        var distance = targetVec.length();
+        var step = targetVec.normalize().scale(Math.min(0.5, distance));
+
+        if (distance < 0.01) {
+            setDeltaMovement(Vec3.ZERO);
+            setPos(targetPosition.x, targetPosition.y, targetPosition.z);
+        } else {
+            setDeltaMovement(step);
+            move(net.minecraft.world.entity.MoverType.SELF, step);
+        }
+
+        setYRot(owner.getYRot());
+        setXRot(0);
+        setRot(getYRot(), getXRot());
+        hasImpulse = true;
+    }
+
+    public static Vec3 computeBehindPos(LivingEntity owner, double backOffSet,double xOffset, double yOffset) {
+        var yawDeg = owner.getYRot();
+        var yawRad = yawDeg * Mth.DEG_TO_RAD;
+        var forwardX = -Mth.sin(yawRad);
+        var forwardZ =  Mth.cos(yawRad);
+
+        var back = new Vec3(-forwardX, 0, -forwardZ).normalize();
+        var right = new Vec3(back.z, 0, -back.x).normalize();
+
+        var behindOffset = back.scale(backOffSet).add(new Vec3(0, yOffset, 0)).add(right.scale(xOffset));
+        return owner.getEyePosition().add(behindOffset);
     }
 }
