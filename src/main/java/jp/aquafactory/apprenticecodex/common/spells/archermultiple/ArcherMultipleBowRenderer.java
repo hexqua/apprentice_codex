03fb01897e242854eb23e965907195fd5679c277
Aquid <hexqua.limit@gmail.com>
2026-01-27 23:21:30 +0900

    未使用のimportを削除

--- src/main/java/jp/aquafactory/apprenticecodex/common/spells/archermultiple/ArcherMultipleBowRenderer.java
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/archermultiple/ArcherMultipleBowRenderer.java
@@ -9,7 +9,6 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
