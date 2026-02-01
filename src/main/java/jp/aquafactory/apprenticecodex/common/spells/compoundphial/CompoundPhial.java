742a2cb648349806a779eb17cb67500fe0b6f0a5
Aquid <hexqua.limit@gmail.com>
2026-02-01 20:59:56 +0900

    次の魔法のたたき台を追加

---
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/compoundphial/CompoundPhial.java
@@ -0,0 +1,94 @@
+package jp.aquafactory.apprenticecodex.common.spells.compoundphial;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CompoundPhial extends AbstractSpell {
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "compound_phial");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.COMMON)
+            .setSchoolResource(SchoolRegistry.NATURE_RESOURCE)
+            .setMaxLevel(10)
+            .setCooldownSeconds(1)
+            .build();
+
+    public CompoundPhial() {
+        // スペルパワー100 = 1ダメージ.
+        // todo:バランス調整.
+        baseSpellPower = 100;
+        spellPowerPerLevel = 15;
+        manaCostPerLevel = 4;
+        baseManaCost = 10;
+        castTime = 10;
+    }
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.damage", Utils.stringTruncation(getDamage(spellLevel, caster), 2)),
+                Component.literal(ApprenticeCodex.NAME)
+        );
+    }
+
+    private float getDamage(int spellLevel, LivingEntity entity) {
+        // スペルパワーはintのため、設定値をそもそも100倍として考える.
+        // todo:バランス調整.
+        return getSpellPower(spellLevel, entity) / 100.0f;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.LONG;
+    }
+    @Override
+    public Optional<SoundEvent> getCastStartSound() {
+        return Optional.of(SoundEvents.BREWING_STAND_BREW);
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        return Optional.of(SoundEvents.SPLASH_POTION_THROW);
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return SpellAnimations.CHARGE_SPIT_ANIMATION;
+    }
+
+    @Override
+    public AnimationHolder getCastFinishAnimation() {
+        return SpellAnimations.THROW_SINGLE_ITEM;
+    }
+
+    @Override
+    public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        // todo:実装.
+
+        super.onCast(level, spellLevel, entity, castSource, playerMagicData);
+    }
+}
