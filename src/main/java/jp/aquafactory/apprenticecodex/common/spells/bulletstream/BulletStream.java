bca8bb6df4f3ad53ba0e44452ff3582fcb082732
Aquid <hexqua.limit@gmail.com>
2026-02-05 13:07:56 +0900

    次の魔法のたたき台を追加

---
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/bulletstream/BulletStream.java
@@ -0,0 +1,125 @@
+package jp.aquafactory.apprenticecodex.common.spells.bulletstream;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.CastType;
+import io.redspace.ironsspellbooks.api.spells.SpellAnimations;
+import io.redspace.ironsspellbooks.api.spells.SpellRarity;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import jp.aquafactory.apprenticecodex.common.registry.EntityRegistry;
+import jp.aquafactory.apprenticecodex.common.spells.AbstractFirearmSpell;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BulletStream extends AbstractFirearmSpell<BulletStreamMinigunEntity> {
+
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "bullet_stream");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.EPIC)
+            .setSchoolResource(SchoolRegistry.LIGHTNING_RESOURCE)
+            .setMaxLevel(3)
+            .setCooldownSeconds(20)
+            .build();
+
+    public BulletStream() {
+        super(BulletStreamMinigunEntity.class);
+        // todo:バランス調整.
+        baseSpellPower = 100;
+        spellPowerPerLevel = 50;
+        manaCostPerLevel = 10;
+        baseManaCost = 20;
+        castTime = 200;
+    }
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.damage", Utils.stringTruncation(getDamage(spellLevel, caster), 2)),
+                Component.literal(ApprenticeCodex.NAME)
+        );
+    }
+
+    private float getDamage(int spellLevel, LivingEntity entity) {
+        // スペルパワーはintのため、設定値をそもそも100倍として考える.
+        // todo:バランス調整.
+        return 2;
+    }
+
+    private int getRange() {
+        // todo:バランス調整.
+        return 16 * 4;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.CONTINUOUS;
+    }
+
+    @Override
+    public int getEffectiveCastTime(int spellLevel, @Nullable LivingEntity entity) {
+        // スキルパワーで詠唱時間を長くする.
+        // todo:バランス調整
+        return Math.round(super.getEffectiveCastTime(spellLevel, entity) * getSpellPower(spellLevel, entity) / 100.0f);
+    }
+
+    @Override
+    public final Optional<SoundEvent> getCastStartSound() {
+        return Optional.of(SoundEvents.ENDERMAN_TELEPORT);
+    }
+
+    @Override
+    public final Optional<SoundEvent> getCastFinishSound() {
+        return Optional.empty();
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return SpellAnimations.ANIMATION_CONTINUOUS_CAST_ONE_HANDED;
+    }
+
+    @Override
+    public AnimationHolder getCastFinishAnimation() {
+        return AnimationHolder.pass();
+    }
+
+    @Override
+    public BulletStreamMinigunEntity onCastNoWeapon(Level level, int spellLevel, LivingEntity entity, MagicData playerMagicData) {
+        var summonWeapon = new BulletStreamMinigunEntity(EntityRegistry.BULLET_STREAM_MINIGUN.get(), level, entity);
+        level.addFreshEntity(summonWeapon);
+        return summonWeapon;
+    }
+
+    @Override
+    public TickCastTypes onCastTickWithWeapon(Level level, int spellLevel, LivingEntity entity, MagicData playerMagicData, @NotNull BulletStreamMinigunEntity weapon) {
+        return TickCastTypes.KEEP_CASTING;
+    }
+
+    @Override
+    public CompleteCastTypes onCastCompleteWithWeapon(Level level, int spellLevel, LivingEntity entity, MagicData playerMagicData, boolean cancelled, @NotNull BulletStreamMinigunEntity weapon) {
+        return CompleteCastTypes.RELEASE_WEAPON;
+    }
+}
---
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/bulletstream/BulletStreamMinigunEntity.java
@@ -0,0 +1,96 @@
+package jp.aquafactory.apprenticecodex.common.spells.bulletstream;
+
+import jp.aquafactory.apprenticecodex.common.entity.spell.SummonWeaponEntity;
+import jp.aquafactory.apprenticecodex.common.utility.CombatTools;
+import jp.aquafactory.apprenticecodex.common.utility.EffectTools;
+import jp.aquafactory.apprenticecodex.common.utility.RaycastTools;
+import jp.aquafactory.apprenticecodex.common.utility.RotationTools;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+public class BulletStreamMinigunEntity extends SummonWeaponEntity {
+
+    public BulletStreamMinigunEntity(EntityType<?> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public BulletStreamMinigunEntity(EntityType<?> pEntityType, Level pLevel, LivingEntity owner) {
+        super(pEntityType, pLevel, owner);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        // do nothing.
+    }
+
+    @Override
+    public void onClientRemoval() {
+        var level = level();
+        EffectTools.createStickParticle(
+                position(),
+                getLookAngle(),
+                1.5,
+                12,
+                0.1f,
+                0.02,
+                ParticleTypes.END_ROD,
+                level
+        );
+
+        super.onClientRemoval();
+    }
+
+    @Override
+    public void tick() {
+        var level = level();
+
+        // 射出時パーティクル(再ログインで消えるので制御不要)
+        if (level.isClientSide && firstTick) {
+            EffectTools.createRingParticle(
+                    position(),
+                    getLookAngle(),
+                    0.2f,
+                    8,
+                    0.01f,
+                    0.01,
+                    ParticleTypes.END_ROD,
+                    level
+            );
+        }
+
+        super.tick();
+
+        if (level.isClientSide) {
+            return;
+        }
+
+        if (!(getOwner() instanceof LivingEntity owner)) {
+            discard();
+            return;
+        }
+
+        var locatePosition = getStandbyPosition();
+        followTargetPosition(locatePosition);
+
+        // 常に視線先を狙う.
+        var aimResult = RaycastTools.raycastFromEye(owner, 64, 1, e -> CombatTools.isValidCombatTarget(e, this));
+        var targetVec = aimResult.hitPosition().subtract(position());
+        var yawPitch = RotationTools.calculateYawPitchByDirection(targetVec);
+        setYRot(yawPitch.yaw());
+        setXRot(yawPitch.pitch());
+        hasImpulse = true;
+    }
+
+    @Override
+    public Vec3 getStandbyPosition() {
+        if ((getOwner() instanceof LivingEntity owner)) {
+            return RotationTools.calculateBehindPosition(owner, -0.3, 1.0, -0.7);
+        }
+
+        return Vec3.ZERO;
+    }
+}
+
