104be13d720cd3e19ba6bc7c9adcb62e39aab966
Aquid <hexqua.limit@gmail.com>
2026-02-06 21:23:09 +0900

    バレットストリーム用ミニガンはアイテムとして不要になったので諸々を削除

--- src/main/java/jp/aquafactory/apprenticecodex/common/registry/ItemRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/common/registry/ItemRegistry.java
@@ -33,10 +33,6 @@ public final class ItemRegistry {
             ITEMS.register("breaching_enemy_shotgun",
                     () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BULLET_STREAM_MINIGUN =
-            ITEMS.register("bullet_stream_minigun",
-                    () -> new Item(new Item.Properties()));
-
     private ItemRegistry() {
         // do nothing.
     }
