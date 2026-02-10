9ca9a456822cce156c88574f3288b4fa750de835
Aquid <hexqua.limit@gmail.com>
2026-02-10 18:45:46 +0900

    先に次の魔法のたたき台を追加

--- src/main/java/jp/aquafactory/apprenticecodex/common/registry/SpellsRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/common/registry/SpellsRegistry.java
@@ -11,6 +11,7 @@ import jp.aquafactory.apprenticecodex.common.spells.bulletstream.BulletStream;
 import jp.aquafactory.apprenticecodex.common.spells.commencefire.CommenceFire;
 import jp.aquafactory.apprenticecodex.common.spells.compoundphial.CompoundPhial;
 import jp.aquafactory.apprenticecodex.common.spells.gracedrain.GracedRain;
+import jp.aquafactory.apprenticecodex.common.spells.magelight.MageLight;
 import jp.aquafactory.apprenticecodex.common.spells.quickarms.QuickArms;
 import jp.aquafactory.apprenticecodex.common.spells.skyedge.SkyEdge;
 import jp.aquafactory.apprenticecodex.common.spells.tinylumberjack.TinyLumberjack;
@@ -54,6 +55,9 @@ public class SpellsRegistry {
 
     public static final RegistryObject<AbstractSpell> GRACED_RAIN =
             SPELLS.register("graced_rain", GracedRain::new);
+
+    public static final RegistryObject<AbstractSpell> MAGE_LIGHT =
+            SPELLS.register("mage_light", MageLight::new);
 
     public static void register(IEventBus bus) {
         SPELLS.register(bus);
---
+++ src/main/java/jp/aquafactory/apprenticecodex/common/spells/magelight/MageLight.java
@@ -0,0 +1,90 @@
+package jp.aquafactory.apprenticecodex.common.spells.magelight;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MageLight extends AbstractSpell {
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "mage_light");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.RARE)
+            .setSchoolResource(SchoolRegistry.HOLY_RESOURCE)
+            .setMaxLevel(1)
+            .setCooldownSeconds(0.5)
+            .build();
+
+    public MageLight() {
+        baseSpellPower = 100;
+        spellPowerPerLevel = 0;
+        baseManaCost = 10;
+        manaCostPerLevel = 0;
+        castTime = 0;
+    }
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.damage", Utils.stringTruncation(getDamage(spellLevel, caster), 2)),
+                Component.literal(ApprenticeCodex.NAME)
+        );
+    }
+
+    private float getDamage(int spellLevel, LivingEntity entity) {
+        return getSpellPower(spellLevel, entity) /10.0f;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastStartSound() {
+        // todo:それっぽい音を割り当てる.
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        return Optional.empty();
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return AnimationHolder.pass();
+    }
+
+    @Override
+    public AnimationHolder getCastFinishAnimation() {
+        return SpellAnimations.ANIMATION_INSTANT_CAST;
+    }
+
+    @Override
+    public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        super.onCast(level, spellLevel, entity, castSource, playerMagicData);
+    }
+}
