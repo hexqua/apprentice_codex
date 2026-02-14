28142eec2868841e5d0853e14dc3e0d8f9f57063
Aquid <hexqua.limit@gmail.com>
2026-02-15 00:51:25 +0900

    体力をマナ変換する指輪を実装

--- src/main/java/jp/aquafactory/apprenticecodex/item/curios/ScarletThirst.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/curios/ScarletThirst.java
@@ -1,10 +1,17 @@
 package jp.aquafactory.apprenticecodex.item.curios;
 
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.compat.Curios;
 import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import jp.aquafactory.apprenticecodex.utility.AudioTools;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
@@ -54,12 +61,35 @@ public class ScarletThirst extends Item implements ICurioItem {
             return;
         }
 
-        if (entity.tickCount % 20 != 0) {
+        // 体力がハート2個以下なら動かない.
+        if (entity.getHealth() <= 4f) {
             return;
         }
 
-        // todo:実装本体(今はCuriosが動いているかのテスト)
-        entity.sendSystemMessage(Component.literal("[tick]" + entity.tickCount));
+        var magicData = MagicData.getPlayerMagicData(entity);
+        if (magicData == null || !(entity instanceof ServerPlayer player)) {
+            return;
+        }
+
+        var currentMana = magicData.getMana();
+        var maxMana = (float) player.getAttributeValue(AttributeRegistry.MAX_MANA.get());
+        var manaRetio = currentMana / maxMana;
+
+        // クルーズ(常時稼働イメージ、弱め、1秒おき)
+        if (entity.tickCount % 20 == 0 && manaRetio <= 0.5f) {
+            // ハート0.5→50マナ.
+            magicData.addMana(50);
+            drainHealthSilentNoKill(entity, 1f);
+            AudioTools.playSoundFromEntity(level, entity, SoundEvents.PLAYER_HURT_DROWN, SoundSource.PLAYERS);
+        }
+
+        // パニック(緊急時稼働イメージ、復帰優先で効率悪め、0.5秒おき)
+        if (entity.tickCount % 10 == 5 && manaRetio <= 0.15f) {
+            // ハート1→100マナ.
+            magicData.addMana(100);
+            drainHealthSilentNoKill(entity, 2f);
+            AudioTools.playSoundFromEntity(level, entity, SoundEvents.PLAYER_HURT_DROWN, SoundSource.PLAYERS);
+        }
     }
 
     @Override
@@ -79,5 +109,11 @@ public class ScarletThirst extends Item implements ICurioItem {
     @Override
     public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
         return true;
+    }
+
+    private static void drainHealthSilentNoKill(LivingEntity entity, float amount) {
+        var newHp = Math.max(1.0f, entity.getHealth() - amount);
+        // todo:mixinで画面揺れを止める.
+        entity.setHealth(newHp);
     }
 }
