12716db62bc49026a9b120411f40c7777ff416e0
Aquid <hexqua.limit@gmail.com>
2026-02-16 16:51:51 +0900

    次の魔法のたたき台を作成

---
+++ src/main/java/jp/aquafactory/apprenticecodex/spell/assistwings/AssistWings.java
@@ -0,0 +1,80 @@
+package jp.aquafactory.apprenticecodex.spell.assistwings;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AssistWings extends AbstractSpell {
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "assist_wings");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.RARE)
+            .setSchoolResource(SchoolRegistry.ENDER_RESOURCE)
+            .setMaxLevel(3)
+            .setCooldownSeconds(0)
+            .build();
+
+    public AssistWings() {
+        baseSpellPower = 100;
+        spellPowerPerLevel = 25;
+        baseManaCost = 20;
+        manaCostPerLevel = 0;
+        castTime = 0;
+    }
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.apprenticecodex.jumps_count", Utils.stringTruncation(getJumpCount(spellLevel, caster), 1)),
+                Component.literal(ApprenticeCodex.NAME)
+        );
+    }
+
+    private int getJumpCount(int spellLevel, LivingEntity entity){
+        // todo:バランス調整.
+        return 5;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        return Optional.of(SoundEvents.HORSE_JUMP);
+    }
+
+    @Override
+    public AnimationHolder getCastFinishAnimation() {
+        return SpellAnimations.ANIMATION_INSTANT_CAST;
+    }
+
+    @Override
+    public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        super.onCast(level, spellLevel, entity, castSource, playerMagicData);
+    }
+}
