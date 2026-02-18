c2009e813b6d5a2707bf8b4ef5ebb8f086bed5d6
Aquid <hexqua.limit@gmail.com>
2026-02-18 15:32:16 +0900

    音は共通化できるため共通化

--- src/main/java/jp/aquafactory/apprenticecodex/registry/SoundRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/registry/SoundRegistry.java
@@ -12,53 +12,23 @@ public final class SoundRegistry {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ApprenticeCodex.MODID);
 
-    public static final RegistryObject<SoundEvent> RIFLE =
-            SOUND_EVENTS.register("rifle",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "rifle")));
+    private static RegistryObject<SoundEvent> reg(String id) {
+        return SOUND_EVENTS.register(id,
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, id)));
+    }
 
-    public static final RegistryObject<SoundEvent> HANDGUN =
-            SOUND_EVENTS.register("handgun",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "handgun")));
-
-    public static final RegistryObject<SoundEvent> SHOTGUN =
-            SOUND_EVENTS.register("shotgun",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "shotgun")));
-
-    public static final RegistryObject<SoundEvent> MINIGUN_SINGLE =
-            SOUND_EVENTS.register("minigun_single",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "minigun_single")));
-
-    public static final RegistryObject<SoundEvent> MINIGUN_LOOP =
-            SOUND_EVENTS.register("minigun_loop",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "minigun_loop")));
-
-    public static final RegistryObject<SoundEvent> MINIGUN_WARMUP =
-            SOUND_EVENTS.register("minigun_warmup",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "minigun_warmup")));
-
-    public static final RegistryObject<SoundEvent> MINIGUN_FINISH =
-            SOUND_EVENTS.register("minigun_finish",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "minigun_finish")));
-
-    public static final RegistryObject<SoundEvent> ARCANE_BLAST =
-            SOUND_EVENTS.register("arcane_blast",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "arcane_blast")));
-
-    public static final RegistryObject<SoundEvent> SAW_START =
-            SOUND_EVENTS.register("saw_start",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "saw_start")));
-
-    public static final RegistryObject<SoundEvent> SAW_STOP =
-            SOUND_EVENTS.register("saw_stop",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "saw_stop")));
-
-    public static final RegistryObject<SoundEvent> CLOUD_RAIN =
-            SOUND_EVENTS.register("cloud_rain",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "cloud_rain")));
-
-    public static final RegistryObject<SoundEvent> SET_MAGE_LIGHT_TORCH =
-            SOUND_EVENTS.register("set_mage_light",
-                    () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "set_mage_light")));
+    public static final RegistryObject<SoundEvent> RIFLE = reg("rifle");
+    public static final RegistryObject<SoundEvent> HANDGUN = reg("handgun");
+    public static final RegistryObject<SoundEvent> SHOTGUN = reg("shotgun");
+    public static final RegistryObject<SoundEvent> MINIGUN_SINGLE = reg("minigun_single");
+    public static final RegistryObject<SoundEvent> MINIGUN_LOOP = reg("minigun_loop");
+    public static final RegistryObject<SoundEvent> MINIGUN_WARMUP = reg("minigun_warmup");
+    public static final RegistryObject<SoundEvent> MINIGUN_FINISH = reg("minigun_finish");
+    public static final RegistryObject<SoundEvent> ARCANE_BLAST = reg("arcane_blast");
+    public static final RegistryObject<SoundEvent> SAW_START = reg("saw_start");
+    public static final RegistryObject<SoundEvent> SAW_STOP = reg("saw_stop");
+    public static final RegistryObject<SoundEvent> CLOUD_RAIN = reg("cloud_rain");
+    public static final RegistryObject<SoundEvent> SET_MAGE_LIGHT_TORCH = reg("set_mage_light");
 
     public static void register(IEventBus modEventBus) {
         SOUND_EVENTS.register(modEventBus);
