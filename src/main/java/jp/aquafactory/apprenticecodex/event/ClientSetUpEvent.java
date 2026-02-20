e13aaea6ef207a9f1b5ab3e504a6b92b5f7d3446
Aquid <hexqua.limit@gmail.com>
2026-02-21 00:19:22 +0900

    エンダーグリモアを装備しても見た目に反映されない不具合を修正

--- src/main/java/jp/aquafactory/apprenticecodex/event/ClientSetUpEvent.java
+++ src/main/java/jp/aquafactory/apprenticecodex/event/ClientSetUpEvent.java
@@ -6,12 +6,14 @@ import jp.aquafactory.apprenticecodex.item.curios.endergrimoire.EnderGrimoireIns
 import jp.aquafactory.apprenticecodex.registry.*;
 import jp.aquafactory.apprenticecodex.renderer.extrudedsprite.ExtrudedSpriteManager;
 import jp.aquafactory.apprenticecodex.spell.personalshelf.PersonalShelfScreen;
+import io.redspace.ironsspellbooks.render.SpellBookCurioRenderer;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,6 +25,7 @@ public final class ClientSetUpEvent {
         event.enqueueWork(() -> MenuScreens.register(MenuRegistry.ENDER_GRIMOIRE_INSCRIPTION.get(), EnderGrimoireInscriptionScreen::new));
         event.enqueueWork(() -> MenuScreens.register(MenuRegistry.PERSONAL_SHELF.get(), PersonalShelfScreen::new));
         event.enqueueWork(ItemRegistry::register);
+        event.enqueueWork(() -> CuriosRendererRegistry.register(ItemRegistry.ENDER_GRIMOIRE.get(), SpellBookCurioRenderer::new));
     }
 
     @SubscribeEvent
