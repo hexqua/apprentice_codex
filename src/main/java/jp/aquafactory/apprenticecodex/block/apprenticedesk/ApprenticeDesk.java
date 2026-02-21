54e4910061a984edd152c08d05c8ba46703eeed4
Aquid <hexqua.limit@gmail.com>
2026-02-21 10:20:37 +0900

    方向を気にする必要がなくなったり手に持った時などがおかしかったのを修正

--- src/main/java/jp/aquafactory/apprenticecodex/block/apprenticedesk/ApprenticeDesk.java
+++ src/main/java/jp/aquafactory/apprenticecodex/block/apprenticedesk/ApprenticeDesk.java
@@ -1,7 +1,6 @@
 package jp.aquafactory.apprenticecodex.block.apprenticedesk;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,25 +8,23 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("deprecation")
-public class ApprenticeDesk extends HorizontalDirectionalBlock {
+public class ApprenticeDesk extends Block {
     private static final Component CONTAINER_TITLE = Component.translatable("container.apprenticecodex.apprentice_desk");
 
     public ApprenticeDesk() {
         super(Properties.of()
                 .strength(2.5f)
-                .sound(SoundType.WOOD));
-
-        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+                .sound(SoundType.WOOD)
+                .noOcclusion());
     }
 
     @Override
@@ -48,26 +45,5 @@ public class ApprenticeDesk extends HorizontalDirectionalBlock {
                 (windowId, inventory, player) -> new ApprenticeDeskMenu(windowId, inventory, ContainerLevelAccess.create(level, pos)),
                 CONTAINER_TITLE
         );
-    }
-
-    @Override
-    @Nullable
-    public BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
-        return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
-    }
-
-    @Override
-    public @NotNull BlockState rotate(@NotNull BlockState state, @NotNull Rotation rotation) {
-        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
-    }
-
-    @Override
-    public @NotNull BlockState mirror(@NotNull BlockState state, @NotNull Mirror mirror) {
-        return state.rotate(mirror.getRotation(state.getValue(FACING)));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
-        builder.add(FACING);
     }
 }
