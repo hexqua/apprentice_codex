e6768aa261b8c3d47c025f2efdf9a2322e50f646
Aquid <hexqua.limit@gmail.com>
2026-02-22 14:47:36 +0900

    杖の演出を追加し、位置を微調整

--- src/main/java/jp/aquafactory/apprenticecodex/item/PastelStaff.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/PastelStaff.java
@@ -2,7 +2,9 @@ package jp.aquafactory.apprenticecodex.item;
 
 import jp.aquafactory.apprenticecodex.renderer.item.PastelStaffRenderer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -12,6 +14,9 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import java.util.function.Consumer;
 
 public class PastelStaff extends Item implements GeoItem {
+    public static final String STONE_TINT_COLOR_TAG = "StoneTintColor";
+    public static final int DEFAULT_STONE_TINT_COLOR = 0xFFFFFF;
+
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public PastelStaff() {
@@ -37,11 +42,32 @@ public class PastelStaff extends Item implements GeoItem {
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        // アニメーションは後続でレンダラー制御するため、現段階では未登録。
+        // アニメーションは後続でレンダラー制御するため未登録.
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+    public int getStoneTintColor(ItemStack stack) {
+        return readStoneTintColor(stack);
+    }
+
+    public static int readStoneTintColor(ItemStack stack) {
+        if (stack == null || stack.isEmpty()) {
+            return DEFAULT_STONE_TINT_COLOR;
+        }
+
+        var tag = stack.getTag();
+        if (tag == null || !tag.contains(STONE_TINT_COLOR_TAG, Tag.TAG_ANY_NUMERIC)) {
+            return DEFAULT_STONE_TINT_COLOR;
+        }
+
+        return tag.getInt(STONE_TINT_COLOR_TAG) & 0xFFFFFF;
+    }
+
+    public static void writeStoneTintColor(ItemStack stack, int rgb) {
+        stack.getOrCreateTag().putInt(STONE_TINT_COLOR_TAG, rgb & 0xFFFFFF);
     }
 }
--- src/main/java/jp/aquafactory/apprenticecodex/renderer/item/PastelStaffRenderer.java
+++ src/main/java/jp/aquafactory/apprenticecodex/renderer/item/PastelStaffRenderer.java
@@ -1,11 +1,55 @@
 package jp.aquafactory.apprenticecodex.renderer.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import jp.aquafactory.apprenticecodex.item.PastelStaff;
 import jp.aquafactory.apprenticecodex.model.PastelStaffModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
 public class PastelStaffRenderer extends GeoItemRenderer<PastelStaff> {
+    private static final String STONE_TINT_BONE = "stone_tint";
+
+    private float stoneRed = 1.0f;
+    private float stoneGreen = 1.0f;
+    private float stoneBlue = 1.0f;
+
     public PastelStaffRenderer() {
         super(new PastelStaffModel());
+    }
+
+    @Override
+    public void preRender(PoseStack poseStack, PastelStaff animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer,
+                          boolean isReRender, float partialTick, int packedLight, int packedOverlay,
+                          float red, float green, float blue, float alpha) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay,
+                red, green, blue, alpha);
+
+        int stoneColor = animatable.getStoneTintColor(getCurrentItemStack());
+        this.stoneRed = ((stoneColor >> 16) & 0xFF) / 255.0f;
+        this.stoneGreen = ((stoneColor >> 8) & 0xFF) / 255.0f;
+        this.stoneBlue = (stoneColor & 0xFF) / 255.0f;
+    }
+
+    @Override
+    public void renderCubesOfBone(PoseStack poseStack, GeoBone bone, VertexConsumer buffer, int packedLight,
+                                  int packedOverlay, float red, float green, float blue, float alpha) {
+        if (STONE_TINT_BONE.equals(bone.getName())) {
+            red *= this.stoneRed;
+            green *= this.stoneGreen;
+            blue *= this.stoneBlue;
+        }
+
+        super.renderCubesOfBone(poseStack, bone, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    @Override
+    public void doPostRenderCleanup() {
+        super.doPostRenderCleanup();
+        this.stoneRed = 1.0f;
+        this.stoneGreen = 1.0f;
+        this.stoneBlue = 1.0f;
     }
 }
