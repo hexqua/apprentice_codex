1f09bc1377963b292cfb27932d222d1f87020104
Aquid <hexqua.limit@gmail.com>
2026-03-01 01:48:07 +0900

    エンチャント適性とベースの能力を微調整

--- src/main/java/jp/aquafactory/apprenticecodex/item/offhand/GoldSpellAmplifier.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/offhand/GoldSpellAmplifier.java
@@ -3,12 +3,18 @@ package jp.aquafactory.apprenticecodex.item.offhand;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import jp.aquafactory.apprenticecodex.item.AbstractOffhandMagicItem;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.ItemStack;
 
 public class GoldSpellAmplifier extends AbstractOffhandMagicItem {
     public GoldSpellAmplifier() {
         super(
                 "gold_spell_amplifier",
-                bonus(AttributeRegistry.MANA_REGEN, 0.50D, AttributeModifier.Operation.MULTIPLY_BASE)
+                bonus(AttributeRegistry.MANA_REGEN, 0.20, AttributeModifier.Operation.MULTIPLY_BASE)
         );
+    }
+
+    @Override
+    public int getEnchantmentValue(ItemStack stack) {
+        return 22;
     }
 }
--- src/main/java/jp/aquafactory/apprenticecodex/item/offhand/IronSpellAmplifier.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/offhand/IronSpellAmplifier.java
@@ -3,13 +3,19 @@ package jp.aquafactory.apprenticecodex.item.offhand;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import jp.aquafactory.apprenticecodex.item.AbstractOffhandMagicItem;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.ItemStack;
 
 public class IronSpellAmplifier extends AbstractOffhandMagicItem {
     public IronSpellAmplifier() {
         super(
                 "iron_spell_amplifier",
-                bonus(AttributeRegistry.MAX_MANA, 50.0D, AttributeModifier.Operation.ADDITION),
-                bonus(AttributeRegistry.MANA_REGEN, -0.05D, AttributeModifier.Operation.MULTIPLY_BASE)
+                bonus(AttributeRegistry.MAX_MANA, 50.0, AttributeModifier.Operation.ADDITION),
+                bonus(AttributeRegistry.MANA_REGEN, -0.10, AttributeModifier.Operation.MULTIPLY_BASE)
         );
+    }
+
+    @Override
+    public int getEnchantmentValue(ItemStack stack) {
+        return 14;
     }
 }
