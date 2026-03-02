d12d51c4bc7c0bf92e542bde1e4d530aaa778c66
Aquid <hexqua.limit@gmail.com>
2026-03-02 15:41:51 +0900

    フォースフィールドの効果量の設定は攻撃力として扱うと意味が不明なので専用のパラメータとして切り出し
    (cherry picked from commit d64f0c2876814840ba0a20cefcc13e228bce0d63)

--- src/main/java/jp/aquafactory/apprenticecodex/config/ApprenticeCodexServerConfig.java
+++ src/main/java/jp/aquafactory/apprenticecodex/config/ApprenticeCodexServerConfig.java
@@ -6,10 +6,12 @@ public final class ApprenticeCodexServerConfig {
     public static final ModConfigSpec SPEC;
     private static final DamageMultiplierServerConfig DAMAGE_MULTIPLIER_CONFIG;
     private static final ItemsServerConfig ITEMS_CONFIG;
+    private static final SpellsServerConfig SPELLS_CONFIG;
     static {
         var builder = new ModConfigSpec.Builder();
         DAMAGE_MULTIPLIER_CONFIG = DamageMultiplierServerConfig.define(builder, DamageMultiplierKey.values());
         ITEMS_CONFIG = ItemsServerConfig.define(builder);
+        SPELLS_CONFIG = SpellsServerConfig.define(builder);
         SPEC = builder.build();
     }
 
@@ -56,5 +58,8 @@ public final class ApprenticeCodexServerConfig {
         return ITEMS_CONFIG.pastelStaffAmplifyTintedMagicMultiplier();
     }
 
+    public static float forceFieldDrainManaBasePerHit() {
+        return SPELLS_CONFIG.forceFieldDrainManaBasePerHit();
+    }
 }
 
