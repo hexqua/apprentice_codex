96f44e4bea0126aacbb75e779fabd0008e4e6cff
Aquid <hexqua.limit@gmail.com>
2026-03-06 19:25:50 +0900

    流石に使い方が分かりづらいアイテムの説明を追加
    (cherry picked from commit 6836cf21271997a10d21326dd9e16d22ac1ede68)

--- src/main/java/jp/aquafactory/apprenticecodex/item/ArcanumInAJarItem.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/ArcanumInAJarItem.java
@@ -1,6 +1,7 @@
 package jp.aquafactory.apprenticecodex.item;
 
 import jp.aquafactory.apprenticecodex.block.arcanuminajar.ArcanumInAJarBlockEntity;
+import jp.aquafactory.apprenticecodex.compat.jei.IJeiInfoItem;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
@@ -12,7 +13,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class ArcanumInAJarItem extends BlockItem {
+public class ArcanumInAJarItem extends BlockItem implements IJeiInfoItem {
+    private static final String JEI_INFO_KEY_PREFIX = "jei.apprenticecodex.arcanum_in_a_jar.desc_";
+
     public ArcanumInAJarItem(Block block, Properties properties) {
         super(block, properties);
     }
@@ -27,5 +30,10 @@ public class ArcanumInAJarItem extends BlockItem {
 
         lines.add(Component.translatable("block.apprenticecodex.arcanum_in_a_jar.fill")
                 .withStyle(ChatFormatting.GRAY));
+    }
+
+    @Override
+    public String getJeiInfoTranslationKeyPrefix() {
+        return JEI_INFO_KEY_PREFIX;
     }
 }
