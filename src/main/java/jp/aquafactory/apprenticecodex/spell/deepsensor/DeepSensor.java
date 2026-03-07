24f1b8f452bbbc1aba96a2c66961e5d41103b130
Aquid <hexqua.limit@gmail.com>
2026-03-08 00:26:32 +0900

    スカルクセンサーを模倣する処理を追加

--- src/main/java/jp/aquafactory/apprenticecodex/spell/deepsensor/DeepSensor.java
+++ src/main/java/jp/aquafactory/apprenticecodex/spell/deepsensor/DeepSensor.java
@@ -5,13 +5,19 @@ import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.spells.*;
 import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
 import jp.aquafactory.apprenticecodex.ApprenticeCodex;
-import jp.aquafactory.apprenticecodex.registry.SoundRegistry;
+import jp.aquafactory.apprenticecodex.registry.EffectRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 
+import java.util.List;
 import java.util.Optional;
 
 public class DeepSensor extends AbstractSpell {
@@ -20,16 +26,38 @@ public class DeepSensor extends AbstractSpell {
     private final DefaultConfig config = new DefaultConfig()
             .setMinRarity(SpellRarity.LEGENDARY)
             .setSchoolResource(SchoolRegistry.ELDRITCH_RESOURCE)
-            .setMaxLevel(1)
+            .setMaxLevel(3)
             .setCooldownSeconds(60)
             .build();
 
     public DeepSensor() {
         baseSpellPower = 100;
-        spellPowerPerLevel = 50;
+        spellPowerPerLevel = 100;
         baseManaCost = 100;
         manaCostPerLevel = 50;
         castTime = 0;
+    }
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.distance", Utils.stringTruncation(getRange(spellLevel, caster), 1)),
+                Component.translatable("ui.irons_spellbooks.effect_length", Utils.timeFromTicks(getDuration(spellLevel, caster), 1))
+        );
+    }
+
+    private int getRange(int spellLevel, LivingEntity caster) {
+        // MobEffectのレベルで制御しているため.
+        return 8 * (1 + getAmplify(spellLevel, caster));
+    }
+
+    private int getAmplify(int spellLevel, LivingEntity caster) {
+        // 初期状態でAmp=0になるように調整.
+        return Math.round(getSpellPower(spellLevel, caster) / 100.0f) - 1;
+    }
+
+    private int getDuration(int spellLevel, LivingEntity caster) {
+        return 20 * 20 + Math.round(5 * 20 * getSpellPower(spellLevel, caster) / 100.0f);
     }
 
     @Override
@@ -49,7 +77,7 @@ public class DeepSensor extends AbstractSpell {
 
     @Override
     public Optional<SoundEvent> getCastFinishSound() {
-        return Optional.of(SoundRegistry.PALETTE.get());
+        return Optional.of(SoundEvents.SCULK_SENSOR_PLACE);
     }
 
     @Override
@@ -59,6 +87,14 @@ public class DeepSensor extends AbstractSpell {
 
     @Override
     public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        entity.addEffect(new MobEffectInstance(
+                EffectRegistry.SENSE_SENSOR.get(),
+                getDuration(spellLevel, entity),
+                getAmplify(spellLevel, entity),
+                false,
+                false,
+                true
+        ));
         super.onCast(level, spellLevel, entity, castSource, playerMagicData);
     }
 }
