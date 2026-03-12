b7caa83d1b2331814323096a69bf00994a59c317
Aquid <hexqua.limit@gmail.com>
2026-03-13 00:03:58 +0900

    ポーチ装備時に表示するように対応
    (cherry picked from commit f9cdc8ba1f7b2b792fb404c2e2fcf365aa352e24)

--- src/main/java/jp/aquafactory/apprenticecodex/event/client/ClientModBusEvents.java
+++ src/main/java/jp/aquafactory/apprenticecodex/event/client/ClientModBusEvents.java
@@ -16,6 +16,7 @@ import jp.aquafactory.apprenticecodex.registry.EntityRegistry;
 import jp.aquafactory.apprenticecodex.registry.ItemRegistry;
 import jp.aquafactory.apprenticecodex.registry.MenuRegistry;
 import jp.aquafactory.apprenticecodex.registry.ParticleRegistry;
+import jp.aquafactory.apprenticecodex.renderer.curio.SpellcasterAmmoPouchCurioRenderer;
 import jp.aquafactory.apprenticecodex.renderer.extrudedsprite.ExtrudedSpriteManager;
 import jp.aquafactory.apprenticecodex.renderer.item.CopperSpellcasterGunRenderer;
 import jp.aquafactory.apprenticecodex.renderer.item.DiamondSpellcasterGunRenderer;
@@ -83,6 +84,7 @@ public final class ClientModBusEvents {
     private static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> CuriosRendererRegistry.register(ItemRegistry.ENDER_GRIMOIRE.get(), SpellBookCurioRenderer::new));
         event.enqueueWork(() -> CuriosRendererRegistry.register(ItemRegistry.EXPLORERS_CODEX.get(), SpellBookCurioRenderer::new));
+        event.enqueueWork(() -> CuriosRendererRegistry.register(ItemRegistry.SPELLCASTER_AMMO_POUCH.get(), SpellcasterAmmoPouchCurioRenderer::new));
         event.enqueueWork(() -> ItemBlockRenderTypes.setRenderLayer(BlockRegistry.ESSENCE_SMOKER.get(), RenderType.cutout()));
     }
 
