28906f4e64168e3bd284c9431d419384059e9202
Aquid <hexqua.limit@gmail.com>
2026-03-19 02:46:40 +0900

    細かい値の調整やエンチャント対応
    (cherry picked from commit 5a43afda9e63a0f67d78d3e000b3fe44604dda9b)

--- src/main/java/jp/aquafactory/apprenticecodex/item/shield/ReflectcastShield.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/shield/ReflectcastShield.java
@@ -7,19 +7,20 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
 public class ReflectcastShield extends AbstractImbueShieldItem implements GeoItem {
+    private static final int ENCHANTMENT_VALUE = 1;
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public ReflectcastShield() {
-        super(new Item.Properties().stacksTo(1).durability(336).rarity(Rarity.COMMON));
+        super(new Item.Properties().stacksTo(1).durability(336).rarity(Rarity.UNCOMMON));
         GeoItem.registerSyncedAnimatable(this);
     }
 
@@ -27,6 +28,16 @@ public class ReflectcastShield extends AbstractImbueShieldItem implements GeoIte
     public boolean isValidRepairItem(@NotNull ItemStack toRepair, @NotNull ItemStack repair) {
         return repair.is(io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_ESSENCE.get())
                 || super.isValidRepairItem(toRepair, repair);
+    }
+
+    @Override
+    public boolean isEnchantable(@NotNull ItemStack stack) {
+        return getEnchantmentValue(stack) > 0;
+    }
+
+    @Override
+    public int getEnchantmentValue(ItemStack stack) {
+        return ENCHANTMENT_VALUE;
     }
 
     @Override
