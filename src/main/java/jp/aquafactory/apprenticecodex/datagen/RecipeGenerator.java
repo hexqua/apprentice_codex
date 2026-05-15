40afcdded82522d0df71d8bde66a0f1b470e2f4e
Aquid <hexqua.limit@gmail.com>
2026-05-16 00:04:37 +0900

    安楽の果実が最序盤食料として強すぎたため、実質中間素材化
    (cherry picked from commit efb6b021664b23b2e83da39055cea70612f05798)

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/RecipeGenerator.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/RecipeGenerator.java
@@ -113,6 +113,12 @@ public final class RecipeGenerator extends RecipeProvider {
                 .unlockedBy(getHasName(Items.BOW), has(Items.BOW))
                 .save(recipeOutput);
 
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ItemRegistry.COMFORT_SANDWICH.get())
+                .requires(Items.BREAD)
+                .requires(ItemRegistry.COMFORT_BERRIES.get(), 2)
+                .unlockedBy(getHasName(ItemRegistry.COMFORT_BERRIES.get()), has(ItemRegistry.COMFORT_BERRIES.get()))
+                .save(recipeWriter);
+
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemRegistry.SCARLET_THIRST.get())
                 .pattern("VI ")
                 .pattern("IMI")
--- src/main/java/jp/aquafactory/apprenticecodex/registry/CreativeTabRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/registry/CreativeTabRegistry.java
@@ -47,6 +47,7 @@ public final class CreativeTabRegistry {
         output.accept(ItemRegistry.ATELIER_STATION.get());
         output.accept(ItemRegistry.ARCANE_CINDER.get());
         output.accept(ItemRegistry.COMFORT_BERRIES.get());
+        output.accept(ItemRegistry.COMFORT_SANDWICH.get());
         output.accept(ItemRegistry.RAPID_SPELLCASTER_ROUND.get());
         output.accept(ItemRegistry.EMPTY_RAPID_SPELLCASTER_CASING.get());
         output.accept(ItemRegistry.BASIC_SPELLCASTER_ROUND.get());
