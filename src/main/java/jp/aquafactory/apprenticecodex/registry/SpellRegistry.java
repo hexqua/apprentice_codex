ce7ac6c3ef6ef1e813fa3aa4271c9af27eee5a49
Aquid <hexqua.limit@gmail.com>
2026-05-20 00:49:08 +0900

    次のアイテムを作るためのリソース類を仮で導入

--- src/main/java/jp/aquafactory/apprenticecodex/registry/SpellRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/registry/SpellRegistry.java
@@ -16,6 +16,7 @@ import jp.aquafactory.apprenticecodex.spell.compoundphial.CompoundPhial;
 import jp.aquafactory.apprenticecodex.spell.deepsensor.DeepSensor;
 import jp.aquafactory.apprenticecodex.spell.demicreatorwings.DemicreatorWings;
 import jp.aquafactory.apprenticecodex.spell.earthforge.EarthForge;
+import jp.aquafactory.apprenticecodex.spell.echocast.EchoCast;
 import jp.aquafactory.apprenticecodex.spell.extract.Extract;
 import jp.aquafactory.apprenticecodex.spell.featherrush.FeatherRush;
 import jp.aquafactory.apprenticecodex.spell.flyswatter.FlySwatter;
@@ -147,4 +148,5 @@ public final class SpellRegistry {
     public static final RegistryObject<AbstractSpell> MOON_LIGHT = reg("moon_light", MoonLight::new);
     public static final RegistryObject<AbstractSpell> DEEP_SENSOR = reg("deep_sensor", DeepSensor::new);
     public static final RegistryObject<AbstractSpell> SPECTRAL_WING = reg("spectral_wing", SpectralWing::new);
+    public static final RegistryObject<AbstractSpell> ECHO_CAST = reg("echo_cast", EchoCast::new);
 }
