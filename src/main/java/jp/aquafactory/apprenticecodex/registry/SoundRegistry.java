3e2689b0ec41dc219d2e5ab460de0401d1bdb9b4
Aquid <hexqua.limit@gmail.com>
2026-06-03 16:26:45 +0900

    術式回転詠唱の杖の演出を実装
    (cherry picked from commit b3d40004bdeab1f5e7d401e05d5cf0ea4e69beb3)

--- src/main/java/jp/aquafactory/apprenticecodex/registry/SoundRegistry.java
+++ src/main/java/jp/aquafactory/apprenticecodex/registry/SoundRegistry.java
@@ -68,6 +68,7 @@ public final class SoundRegistry {
     public static final DeferredHolder<SoundEvent, SoundEvent> ICE_DAGGER_THROW = reg("ice_dagger_throw");
     public static final DeferredHolder<SoundEvent, SoundEvent> ICE_DAGGER_HIT = reg("ice_dagger_hit");
     public static final DeferredHolder<SoundEvent, SoundEvent> KAMI = reg("kami");
+    public static final DeferredHolder<SoundEvent, SoundEvent> REVOLVE = reg("revolve");
     public static final DeferredHolder<SoundEvent, SoundEvent> SMASHCAST_SCEPTER_SMASH_AIR = reg("smashcast_scepter_smash_air");
     public static final DeferredHolder<SoundEvent, SoundEvent> SMASHCAST_SCEPTER_SMASH_GROUND = reg("smashcast_scepter_smash_ground");
     public static final DeferredHolder<SoundEvent, SoundEvent> SMASHCAST_SCEPTER_SMASH_GROUND_HEAVY = reg("smashcast_scepter_smash_ground_heavy");
