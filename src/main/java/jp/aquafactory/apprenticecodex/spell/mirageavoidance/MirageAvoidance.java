22f56b3d7bd2f0d3148a8f7abf4e66638bad4c37
Aquid <hexqua.limit@gmail.com>
2026-06-12 13:24:42 +0900

    次の魔法のたたき台を作成

---
+++ src/main/java/jp/aquafactory/apprenticecodex/spell/mirageavoidance/MirageAvoidance.java
@@ -0,0 +1,81 @@
+package jp.aquafactory.apprenticecodex.spell.mirageavoidance;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MirageAvoidance extends AbstractSpell {
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "mirage_avoidance");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.EPIC)
+            .setSchoolResource(SchoolRegistry.ENDER_RESOURCE)
+            .setMaxLevel(1)
+            .setCooldownSeconds(3)
+            .build();
+
+    public MirageAvoidance() {
+        baseSpellPower = 0;
+        spellPowerPerLevel = 0;
+        baseManaCost = 50;
+        manaCostPerLevel = 0;
+        castTime = 0;
+    }
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.effect_length", Utils.timeFromTicks(getDuration(), 1))
+        );
+    }
+
+    private int getDuration() {
+        // 無敵時間であり、効果時間は別.
+        return 20;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        // todo:字幕用に自前のsounds.jsonに切り出すか音を作る.
+        return Optional.of(SoundEvents.PORTAL_TRAVEL);
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return AnimationHolder.pass();
+    }
+
+    @Override
+    public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        super.onCast(level, spellLevel, entity, castSource, playerMagicData);
+    }
+}
