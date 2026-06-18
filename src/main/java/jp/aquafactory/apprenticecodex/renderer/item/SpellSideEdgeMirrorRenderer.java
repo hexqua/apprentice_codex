2a4c30a05b73cf985aba7872cc783fd6df916399
Aquid <hexqua.limit@gmail.com>
2026-06-19 00:29:56 +0900

    詠唱補助の刃関連のテクスチャと描画を調整

--- src/main/java/jp/aquafactory/apprenticecodex/renderer/item/SpellSideEdgeMirrorRenderer.java
+++ src/main/java/jp/aquafactory/apprenticecodex/renderer/item/SpellSideEdgeMirrorRenderer.java
@@ -2,9 +2,8 @@ package jp.aquafactory.apprenticecodex.renderer.item;
 
 import jp.aquafactory.apprenticecodex.item.spellsideedge.SpellSideEdgeMirror;
 import jp.aquafactory.apprenticecodex.model.SpellSideEdgeMirrorModel;
-import software.bernie.geckolib.renderer.GeoItemRenderer;
 
-public final class SpellSideEdgeMirrorRenderer extends GeoItemRenderer<SpellSideEdgeMirror> {
+public final class SpellSideEdgeMirrorRenderer extends AbstractSpellSideEdgeRenderer<SpellSideEdgeMirror> {
     public SpellSideEdgeMirrorRenderer() {
         super(new SpellSideEdgeMirrorModel());
     }
--- src/main/java/jp/aquafactory/apprenticecodex/renderer/item/SpellSideEdgeRenderer.java
+++ src/main/java/jp/aquafactory/apprenticecodex/renderer/item/SpellSideEdgeRenderer.java
@@ -2,9 +2,8 @@ package jp.aquafactory.apprenticecodex.renderer.item;
 
 import jp.aquafactory.apprenticecodex.item.spellsideedge.SpellSideEdge;
 import jp.aquafactory.apprenticecodex.model.SpellSideEdgeModel;
-import software.bernie.geckolib.renderer.GeoItemRenderer;
 
-public final class SpellSideEdgeRenderer extends GeoItemRenderer<SpellSideEdge> {
+public final class SpellSideEdgeRenderer extends AbstractSpellSideEdgeRenderer<SpellSideEdge> {
     public SpellSideEdgeRenderer() {
         super(new SpellSideEdgeModel());
     }
