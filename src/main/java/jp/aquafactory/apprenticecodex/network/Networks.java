3b57333080fc811848571c120d3f806c3a9811fd
Aquid <hexqua.limit@gmail.com>
2026-07-08 00:33:02 +0900

    リモートアイの効果時間をクライアントに送信していないのを修正

--- src/main/java/jp/aquafactory/apprenticecodex/network/Networks.java
+++ src/main/java/jp/aquafactory/apprenticecodex/network/Networks.java
@@ -63,7 +63,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 import java.util.Optional;
 
 public final class Networks {
-    private static final String PROTOCOL_VERSION = "53";
+    private static final String PROTOCOL_VERSION = "54";
     private static int nextPacketId = 0;
 
     private static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
