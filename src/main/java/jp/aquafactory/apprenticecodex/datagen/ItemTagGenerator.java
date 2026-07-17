7c816ce232af747ce59aecbf78501a97877c953c
Aquid <hexqua.limit@gmail.com>
2026-07-17 12:23:17 +0900

    魔法性能を上げるタイプのエンチャントタグを用意し、forward-port時のリグレッション回避をしやすいように対応
    (cherry picked from commit 759e23459d82712e7ba2179f44c956008fdddaed)

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/ItemTagGenerator.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/ItemTagGenerator.java
@@ -21,6 +21,7 @@ import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public final class ItemTagGenerator extends ItemTagsProvider {
