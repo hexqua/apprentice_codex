efda13cc2c5783ea129a3b734df66b32427dcb10
Aquid <hexqua.limit@gmail.com>
2026-07-24 16:45:14 +0900

    次の魔法のたたき台及びアイテム用機能アイコンを追加

---
+++ src/main/java/jp/aquafactory/apprenticecodex/spell/wizardlamp/Wizardlamp.java
@@ -0,0 +1,80 @@
+package jp.aquafactory.apprenticecodex.spell.wizardlamp;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import jp.aquafactory.apprenticecodex.ApprenticeCodex;
+import jp.aquafactory.apprenticecodex.registry.SoundRegistry;
+import jp.aquafactory.apprenticecodex.spell.IChargecastStaffbowIncompatibleSpell;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Wizardlamp extends AbstractSpell implements IChargecastStaffbowIncompatibleSpell {
+    private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "wizardlamp");
+
+    private final DefaultConfig config = new DefaultConfig()
+            .setMinRarity(SpellRarity.RARE)
+            .setSchoolResource(SchoolRegistry.HOLY_RESOURCE)
+            .setMaxLevel(1)
+            .setCooldownSeconds(4)
+            .build();
+
+    public Wizardlamp() {
+        baseSpellPower = 0;
+        spellPowerPerLevel = 0;
+        baseManaCost = 50;
+        manaCostPerLevel = 0;
+        castTime = 0;
+    }
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(
+                Component.translatable("ui.irons_spellbooks.distance", Utils.stringTruncation(getRange(), 1))
+        );
+    }
+
+    private double getRange(){
+        return 6;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return config;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        return Optional.of(SoundRegistry.SET_MAGE_LIGHT_TORCH.get());
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return SpellAnimations.ANIMATION_INSTANT_CAST;
+    }
+
+    @Override
+    public void onCast(Level level, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        super.onCast(level, spellLevel, entity, castSource, playerMagicData);
+    }
+}
