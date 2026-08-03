ecd803e56753672a4fa882120986dfed21f13eac
Aquid <hexqua.limit@gmail.com>
2026-08-03 22:54:46 +0900

    使い走り魔術師のlootのバランスを調整
    (cherry picked from commit 387f55c7d9d40429fe0708213074ebbaa612464a)

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/ChestLootTableGenerator.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/ChestLootTableGenerator.java
@@ -5,7 +5,6 @@ import jp.aquafactory.apprenticecodex.loot.RandomSpellImbueLootFunction;
 import jp.aquafactory.apprenticecodex.registry.ItemRegistry;
 import net.minecraft.data.loot.LootTableSubProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -35,20 +34,19 @@ public final class ChestLootTableGenerator implements LootTableSubProvider {
                         new IntRange(3, 5),
                         1.0F,
                         List.of(
-                                entry(Items.BOOK, 10, 1, 3),
-                                entry(Items.BREAD, 5, 1, 4),
-                                entry(ItemRegistry.COMFORT_BERRIES.get(), 5, 1, 5),
-                                entry(ItemRegistry.COMFORT_SANDWICH.get(), 1, 1, 4),
-                                entry(ItemRegistry.CRUDE_INK.get(), 1, 1, 1)
+                                entry(ItemRegistry.COMFORT_BERRIES.get(), 5, 1, 3),
+                                entry(ItemRegistry.COMFORT_SANDWICH.get(), 3, 2, 4),
+                                entry(ItemRegistry.CRUDE_INK.get(), 1, 1, 2),
+                                entry(ItemRegistry.CRYSTALLINE_ARCANE_SHARD.get(), 1, 2, 3)
                         )
                 ),
                 new PoolDefinition(
-                        new IntRange(2, 2),
-                        0.5F,
+                        new IntRange(3, 3),
+                        1.0F,
                         List.of(
                                 entry(
                                         ItemRegistry.WOODEN_WAND.get(),
-                                        1,
+                                        4,
                                         1,
                                         1,
                                         RandomSpellImbueLootFunction.builder(
@@ -56,7 +54,8 @@ public final class ChestLootTableGenerator implements LootTableSubProvider {
                                                 ItemRegistry.CRUDE_INK.get()
                                         )
                                 ),
-                                entry(ItemRegistry.ARCANE_CINDER.get(), 1, 1, 1)
+                                entry(ItemRegistry.ARCANE_CINDER.get(), 1, 1, 1),
+                                emptyEntry(5)
                         )
                 )
         )));
