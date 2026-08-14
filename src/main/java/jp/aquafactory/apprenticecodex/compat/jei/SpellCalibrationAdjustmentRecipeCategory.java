f2856862963d3515c090a111488e8611d4a7bf33
Aquid <hexqua.limit@gmail.com>
2026-08-14 19:55:55 +0900

    JEIの説明文を一つのテキストブロックで行うように、且つ幅の折り返しを行うように修正

--- src/main/java/jp/aquafactory/apprenticecodex/compat/jei/SpellCalibrationAdjustmentRecipeCategory.java
+++ src/main/java/jp/aquafactory/apprenticecodex/compat/jei/SpellCalibrationAdjustmentRecipeCategory.java
@@ -26,8 +26,8 @@ public final class SpellCalibrationAdjustmentRecipeCategory
     private static final int OUTPUT_Y = 2;
     private static final int EFFECT_X = 4;
     private static final int EFFECT_Y = 32;
+    private static final int EFFECT_WIDTH = WIDTH - EFFECT_X * 2;
     private static final int TEXT_LINE_HEIGHT = 11;
-    private static final int CONSTRAINT_Y = 67;
     private static final int TEXT_COLOR = 0x404040;
 
     public SpellCalibrationAdjustmentRecipeCategory(IGuiHelper guiHelper) {
@@ -69,12 +69,14 @@ public final class SpellCalibrationAdjustmentRecipeCategory
     ) {
         drawLabel(guiGraphics, Component.literal("+"), 27, 8);
         recipeArrow.draw(guiGraphics, ARROW_X, ARROW_Y);
-        for (var line = 0; line < Math.min(3, recipe.effectLines().size()); ++line) {
-            drawLabel(guiGraphics, recipe.effectLines().get(line), EFFECT_X, EFFECT_Y + line * TEXT_LINE_HEIGHT);
+        var description = Component.empty().append(recipe.effectLines().getFirst());
+        for (var line = 1; line < recipe.effectLines().size(); ++line) {
+            description.append("\n").append(recipe.effectLines().get(line));
         }
         recipe.constraintDisplay().translationKey()
                 .map(Component::translatable)
-                .ifPresent(constraint -> drawConstraint(guiGraphics, constraint));
+                .ifPresent(constraint -> description.append("\n").append(constraint));
+        drawDescription(guiGraphics, description);
     }
 
     @Override
@@ -82,15 +84,15 @@ public final class SpellCalibrationAdjustmentRecipeCategory
         return recipe.id();
     }
 
-    private static void drawConstraint(GuiGraphics guiGraphics, Component constraint) {
+    private static void drawDescription(GuiGraphics guiGraphics, Component description) {
         var font = Minecraft.getInstance().font;
-        var lines = font.split(constraint, WIDTH - 8);
-        for (var line = 0; line < Math.min(2, lines.size()); ++line) {
+        var lines = font.split(description, EFFECT_WIDTH);
+        for (var line = 0; line < lines.size(); ++line) {
             guiGraphics.drawString(
                     font,
                     lines.get(line),
                     EFFECT_X,
-                    CONSTRAINT_Y + line * TEXT_LINE_HEIGHT,
+                    EFFECT_Y + line * TEXT_LINE_HEIGHT,
                     TEXT_COLOR,
                     false
             );
