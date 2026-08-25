e5955d42aa9f6a9c787034e771ca546bbbb7ac5e
Aquid <hexqua.limit@gmail.com>
2026-08-25 13:34:25 +0900

    磁場安定アンカーのレシピを追加
    (cherry picked from commit 6fb3b097237ebcdcf5c09e2c7b9e848f7a303df3)

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/RecipeGenerator.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/RecipeGenerator.java
@@ -80,6 +80,16 @@ public final class RecipeGenerator extends RecipeProvider {
                 .unlockedBy(getHasName(io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_INGOT.get()), has(io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_INGOT.get()))
                 .save(recipeWriter, ItemRegistry.ARCANUM_IN_A_JAR.getId());
 
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ItemRegistry.MAGNETIC_STABILITY_ANCHOR.get(), 4)
+                .pattern("RAR")
+                .pattern("ACA")
+                .pattern("RAR")
+                .define('A', io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_ESSENCE.get())
+                .define('R', Items.REDSTONE)
+                .define('C', Items.COPPER_INGOT)
+                .unlockedBy(getHasName(io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_ESSENCE.get()), has(io.redspace.ironsspellbooks.registries.ItemRegistry.ARCANE_ESSENCE.get()))
+                .save(recipeWriter);
+
         ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ItemRegistry.ESSENCE_SMOKER.get())
                 .pattern("A A")
                 .pattern("FEF")
