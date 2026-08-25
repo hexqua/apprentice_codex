bf643fbb7b3ee1a2211f35dae4b1f18eee30f089
Aquid <hexqua.limit@gmail.com>
2026-08-25 14:53:31 +0900

    マナ推進装置のマナ回復停止が他アイテムと比較して致命的に弱い要素と成り下がっていたため削除
    (cherry picked from commit 17240199622b87332b7ec86257ca648f10a6f023)

--- src/main/java/jp/aquafactory/apprenticecodex/item/curios/manathruster/ManaThrusterContext.java
+++ src/main/java/jp/aquafactory/apprenticecodex/item/curios/manathruster/ManaThrusterContext.java
@@ -1,7 +1,5 @@
 package jp.aquafactory.apprenticecodex.item.curios.manathruster;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.player.Player;
 
 public final class ManaThrusterContext {
@@ -17,44 +15,5 @@ public final class ManaThrusterContext {
                 || abilities.flying
                 || player.onClimbable()
                 || player.isPassenger();
-    }
-
-    public static boolean isManaRecoveryFree(Player player) {
-        return player.isInWaterOrBubble() || player.isSwimming() || isInLavaOrTouchingLava(player);
-    }
-
-    private static boolean isInLavaOrTouchingLava(Player player) {
-        if (player.isInLava()) {
-            return true;
-        }
-
-        var level = player.level();
-        var box = player.getBoundingBox().deflate(1.0E-4D);
-        var minX = net.minecraft.util.Mth.floor(box.minX);
-        var maxX = net.minecraft.util.Mth.floor(box.maxX);
-        var minY = net.minecraft.util.Mth.floor(box.minY);
-        var maxY = net.minecraft.util.Mth.floor(box.maxY);
-        var minZ = net.minecraft.util.Mth.floor(box.minZ);
-        var maxZ = net.minecraft.util.Mth.floor(box.maxZ);
-
-        var mutablePos = new BlockPos.MutableBlockPos();
-        for (var y = minY; y <= maxY; ++y) {
-            for (var x = minX; x <= maxX; ++x) {
-                for (var z = minZ; z <= maxZ; ++z) {
-                    mutablePos.set(x, y, z);
-                    var fluidState = level.getFluidState(mutablePos);
-                    if (!fluidState.is(FluidTags.LAVA)) {
-                        continue;
-                    }
-
-                    var fluidTop = y + fluidState.getHeight(level, mutablePos);
-                    if (box.maxY > y && box.minY < fluidTop) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
     }
 }
