9ece80a46fb4f19c22b0bf7743b969effc9a4cef
Aquid <hexqua.limit@gmail.com>
2026-09-01 14:05:51 +0900

    クリエイティブ画面のストレージ安定装置操作を同期
    (cherry picked from commit 70a7e05a7b4531e2088a60400f068376da96f8a3)

--- src/main/java/jp/aquafactory/apprenticecodex/network/Networks.java
+++ src/main/java/jp/aquafactory/apprenticecodex/network/Networks.java
@@ -15,6 +15,7 @@ import jp.aquafactory.apprenticecodex.network.packet.ClientFocusStaffbowCancelPa
 import jp.aquafactory.apprenticecodex.network.packet.ClientJumpcastCharmCastPacket;
 import jp.aquafactory.apprenticecodex.network.packet.ClientMirageAvoidanceCastPacket;
 import jp.aquafactory.apprenticecodex.network.packet.ClientOpenSpellcasterAccessoryCasePacket;
+import jp.aquafactory.apprenticecodex.network.packet.ClientOpenStorageStabilizerEnderChestPacket;
 import jp.aquafactory.apprenticecodex.network.packet.ClientManaThrusterInputPacket;
 import jp.aquafactory.apprenticecodex.network.packet.ClientBroomInputPacket;
 import jp.aquafactory.apprenticecodex.network.packet.ClientBroomDismountInputPacket;
@@ -91,7 +92,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 import java.util.Optional;
 
 public final class Networks {
-    private static final String PROTOCOL_VERSION = "91";
+    private static final String PROTOCOL_VERSION = "92";
     private static int nextPacketId = 0;
 
     private static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
@@ -105,6 +106,14 @@ public final class Networks {
     }
 
     public static void register() {
+        CHANNEL.registerMessage(
+                nextPacketId++,
+                ClientOpenStorageStabilizerEnderChestPacket.class,
+                ClientOpenStorageStabilizerEnderChestPacket::encode,
+                ClientOpenStorageStabilizerEnderChestPacket::decode,
+                ClientOpenStorageStabilizerEnderChestPacket::handle,
+                Optional.of(NetworkDirection.PLAY_TO_SERVER)
+        );
         CHANNEL.registerMessage(
                 nextPacketId++,
                 SyncArcanumInAJarConfigPacket.class,
