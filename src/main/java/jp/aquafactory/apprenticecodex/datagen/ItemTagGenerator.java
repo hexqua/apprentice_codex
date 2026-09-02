4d40c67cf1c47c82a45cc6c06007e2e10a0b11fe
Aquid <hexqua.limit@gmail.com>
2026-09-02 18:12:35 +0900

    1.21.1においてMalumのポーチの効率対象煮含める

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/ItemTagGenerator.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/ItemTagGenerator.java
@@ -367,6 +367,7 @@ public final class ItemTagGenerator extends ItemTagsProvider {
                 ItemRegistry.BULWARK_GREATSHIELD.get(),
                 ItemRegistry.MANA_SHIELD_CHARM.get(),
                 ItemRegistry.MONARCH_BOND_CHARM.get(),
+                ItemRegistry.UNDYING_EMBLEM.get(),
                 ItemRegistry.ASHEN_CIRCLET.get(),
                 ItemRegistry.ENCHANTED_CIRCLET.get(),
                 ItemRegistry.ARCANE_CINDER.get(),
