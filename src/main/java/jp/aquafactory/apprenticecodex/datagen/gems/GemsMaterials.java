aebbeb5e94411edd59446dd51a91769d0ea83056
Aquid <hexqua.limit@gmail.com>
2026-09-16 19:09:23 +0900

    全素材に状態異常耐性を設定

--- src/main/java/jp/aquafactory/apprenticecodex/datagen/gems/GemsMaterials.java
+++ src/main/java/jp/aquafactory/apprenticecodex/datagen/gems/GemsMaterials.java
@@ -1,13 +1,18 @@
 package jp.aquafactory.apprenticecodex.datagen.gems;
 
 import io.redspace.ironsjewelry.core.actions.ApplyEffectAction;
+import io.redspace.ironsjewelry.core.actions.CreateItemsAction;
 import io.redspace.ironsjewelry.core.data.QualityScalar;
 import io.redspace.ironsjewelry.utils.JewelryModTags;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
+import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import jp.aquafactory.apprenticecodex.ApprenticeCodex;
 import jp.aquafactory.apprenticecodex.registry.EffectRegistry;
 import jp.aquafactory.apprenticecodex.registry.ItemRegistry;
+import jp.aquafactory.apprenticecodex.registry.SoundRegistry;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.crafting.Ingredient;
 
@@ -25,28 +30,34 @@ public final class GemsMaterials {
                 material("crystalline_arcane", 0.5)
                         .ingredient(Ingredient.of(ItemRegistry.CRYSTALLINE_ARCANE_SHARD.get()))
                         .attribute(AttributeRegistry.SPELL_POWER, 0.03, operation)
+                        .negativeEffect(MobEffects.WEAKNESS)
                         .tag(JewelryModTags.GEM).build(),
                 material("emberstained_netherite", 2.75)
                         .ingredient(Ingredient.of(ItemRegistry.EMBERSTAINED_NETHERITE_INGOT.get()))
                         .attribute(AttributeRegistry.SPELL_RESIST, 0.1, operation)
+                        .negativeEffect(EffectRegistry.THERMAL_SUNDERED)
                         .tag(JewelryModTags.METAL).build(),
                 material("mana_enveloped_silver", 3)
                         .ingredient(Ingredient.of(ItemRegistry.MANA_ENVELOPED_SILVER_CHUNK.get()))
                         .attribute(AttributeRegistry.MAX_MANA, 0.1, operation)
+                        .negativeEffect(MobEffectRegistry.BLIGHT)
                         .tag(JewelryModTags.METAL).build(),
                 material("spellstained_arcane", 1.75)
                         .ingredient(Ingredient.of(ItemRegistry.SPELLSTAINED_ARCANE_INGOT.get()))
                         .attribute(AttributeRegistry.MANA_REGEN, 0.05, operation)
                         .action(new ApplyEffectAction(new QualityScalar(100),
                                 new QualityScalar(0, 1, 0, Optional.of(2d)), EffectRegistry.MANA_REGENERATION), true)
+                        .negativeEffect(MobEffectRegistry.REND)
                         .tag(JewelryModTags.METAL).build(),
                 material("spellstained_diamond", 2.25)
                         .ingredient(Ingredient.of(ItemRegistry.SPELLSTAINED_DIAMOND.get()))
                         .attribute(AttributeRegistry.MANA_REGEN, 0.15, operation)
+                        .negativeEffect(MobEffectRegistry.SOUL_BURN)
                         .tag(JewelryModTags.GEM).build(),
                 material("wisdom", 2.5)
                         .ingredient(Ingredient.of(ItemRegistry.WISDOM_SHARD.get()))
                         .attribute(AttributeRegistry.MAX_MANA, 0.1, operation)
+                        .negativeEffect(MobEffectRegistry.BLIGHT)
                         .tag(JewelryModTags.GEM).build()
         );
     }
