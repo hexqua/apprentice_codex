32bdfdf5f8c2943d8fdae23d52ba86482772cc2c
Aquid <hexqua.limit@gmail.com>
2026-09-17 13:40:56 +0900

    アイテム専用のエルドリッチ系統の魔法のアンロック画面での非表示を対応

--- src/main/java/jp/aquafactory/apprenticecodex/spell/soulconversion/SoulConversion.java
+++ src/main/java/jp/aquafactory/apprenticecodex/spell/soulconversion/SoulConversion.java
@@ -3,25 +3,30 @@ package jp.aquafactory.apprenticecodex.spell.soulconversion;
 import io.redspace.ironsspellbooks.api.config.DefaultConfig;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
-import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import io.redspace.ironsspellbooks.api.spells.CastSource;
+import io.redspace.ironsspellbooks.api.spells.CastType;
+import io.redspace.ironsspellbooks.api.spells.SpellAnimations;
+import io.redspace.ironsspellbooks.api.spells.SpellRarity;
 import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
 import jp.aquafactory.apprenticecodex.ApprenticeCodex;
-import jp.aquafactory.apprenticecodex.registry.SoundRegistry;
 import jp.aquafactory.apprenticecodex.compat.malum.MalumStaffChargeBridge;
 import jp.aquafactory.apprenticecodex.item.curios.manasoultransducer.ManaSoulTransducerEvents;
-import io.redspace.ironsspellbooks.api.util.Utils;
-import net.minecraft.server.level.ServerPlayer;
+import jp.aquafactory.apprenticecodex.registry.SoundRegistry;
+import jp.aquafactory.apprenticecodex.spell.HiddenFromEldritchResearch;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 import java.util.Optional;
 
-public class SoulConversion extends AbstractSpell {
+public class SoulConversion extends AbstractSpell implements HiddenFromEldritchResearch {
     private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(ApprenticeCodex.MODID, "soul_conversion");
 
     private final DefaultConfig config = new DefaultConfig()
